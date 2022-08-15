5d6a51da8fa49ab96e3ee7633d42890e2f7bd285
Istvan Willmott <istvan.willmott@gmail.com>
2022-08-15 17:39:23 +0930

    Setup main

--- src/main.c
+++ src/main.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
 
+int	main(int argc, char *argv[])
+{
+	if (argc != 5 && argc != 6)
+	{
+		printf("Expected:\n./philo <number_of_philosophers"
+			"> <time_to_die> <time_to_eat> <time_to_sleep> <"
+			"(optional)number_of_times_each_philosopher_"
+			"must_eat>\n");
+	}
+	return (0);
+}

3a8b58eaebb31d0cc2d80e6a8f4bf6482ad89235
Istvan Willmott <istvan.willmott@gmail.com>
2022-08-22 16:27:03 +0930

    Init threads

--- include/philo.h
+++ include/philo.h
@@ -10,4 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+
+typedef struct	s_philo
+{
+	int				num;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				times_eaten;
+	pthread_mutex_t fork;
+}	t_philo;
+
+typedef struct s_brain
+{
+	pthread_t	*thread_id;
+	int			total_philo;
+	t_philo		*philos;
+	int			alive;
+}	t_brain;
+
+int	ft_atoi(const char *str);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -14,12 +14,18 @@
 
 int	main(int argc, char *argv[])
 {
+	t_brain brain;
+
 	if (argc != 5 && argc != 6)
 	{
 		printf("Expected:\n./philo <number_of_philosophers"
 			"> <time_to_die> <time_to_eat> <time_to_sleep> <"
 			"(optional)number_of_times_each_philosopher_"
 			"must_eat>\n");
+		return (1);
 	}
+	brain.total_philo = ft_atoi(argv[1]);
+	brain.philos = malloc(sizeof(t_philo) * brain.total_philo);
+	brain.alive = 1;
 	return (0);
 }
